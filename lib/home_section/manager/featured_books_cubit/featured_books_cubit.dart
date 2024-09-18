d799404d0dc9c99c486af008a6c672db725343e3
aml_gamal <amlgamal@std.mans.edu.eg>
2024-09-18 06:45:15 +0300

    cubit

--- lib/home_section/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/home_section/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly/errors/Failures.dart';
 import 'package:bookly/home_section/Data/models/book_models/book_models.dart';
+import 'package:bookly/home_section/Data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo; // علشان نعرف نناديها
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+
+    result.fold((Failure) {
+      emit(FeaturedBooksFailure(Failure.errMessage));
+    }, (books) {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
